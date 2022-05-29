8a41da93f4a14c1fc0d02a4225edc9a444030d5c
agung <agungdwisaputra0808@gmail.com>
2022-05-29 19:26:06 +0700

    Initial Commit

---
+++ MealsAppDicodyByMe/MealsAppDicodyByMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsAppDicodyByMeApp.swift
+//  MealsAppDicodyByMe
+//
+//  Created by Putra on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MealsAppDicodyByMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
